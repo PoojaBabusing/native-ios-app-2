97ce3b357cf0413b3a8135313e12c2bccbdb84da
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-03-26 14:06:45 +0530

    first commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by Pooja Babusing on 26/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Pooja Babusing on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
